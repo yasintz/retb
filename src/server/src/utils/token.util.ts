4347dc1907147f97bfadfc38f5a8e00613fc7bc1
yasintz <yasinntazeoglu@gmail.com>
2020-03-07 18:23:22 +0300

    update for lint

--- src/server/src/utils/token.util.ts
+++ src/server/src/utils/token.util.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import ServerContext from '@server/context';
 
 function createToken(userId: string, expiresIn: string) {
-  const token = jwt.sign({ userId }, ServerContext.TOKEN_SECRET_KEY, { expiresIn });
+  const token = jwt.sign({ userId }, ServerContext.Config.TOKEN_SECRET_KEY, { expiresIn });
 
   return `Bearer ${token}`;
 }
